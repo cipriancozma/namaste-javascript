8a44e837292bb416342e6c8fed7ecc9b9fa22b9b
Ciprian <cozma_ciprian19@yahoo.com>
2024-03-20 19:01:11 +0200

    added es7

---
+++ es7/es7.js
@@ -0,0 +1,8 @@
+const test = "Hello";
+// console.log(test.includes("e")); // true
+
+const pets = ["cat", "dog", "bat"];
+// console.log(pets.includes("cat")); // true
+
+const square = (num) => num ** 2;
+// console.log(square(5));
