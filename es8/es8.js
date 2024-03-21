ede792a77bd0f9e82136020dacf9a1bf24f99e9b
Ciprian <cozma_ciprian19@yahoo.com>
2024-03-21 07:36:43 +0200

    added es8

---
+++ es8/es8.js
@@ -0,0 +1,24 @@
+const test = "Turtle";
+// console.log(test.padStart(10));
+// console.log(test.padEnd(5));
+
+const obj = {
+  username: "Ciprian",
+  moto: "Grinch",
+};
+
+// console.log(Object.entries(obj));
+// console.log(Object.keys(obj));
+// console.log(Object.values(obj));
+
+// Object.keys(obj).forEach((item) => {
+//   console.log(item + "Hello");
+// });
+
+// Object.values(obj).forEach((item) => {
+//   console.log(item);
+// });
+
+// Object.entries(obj).map((item) => {
+//   console.log(item[0] + " " + item[1] + "-----");
+// });
