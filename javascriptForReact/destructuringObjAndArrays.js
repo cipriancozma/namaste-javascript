6c72bc01e2c8ce8aefb64feeaf6061366ccf1262
Ciprian <cozma_ciprian19@yahoo.com>
2024-04-27 23:46:43 +0300

    finished obj and array destructuring

--- javascriptForReact/destructuringObjAndArrays.js
+++ javascriptForReact/destructuringObjAndArrays.js
@@ -3,26 +3,31 @@ const data = [
     id: 1,
     title: "Book-1",
     author: "Author-1",
+    genres: ["science fiction", "humor", "short stories", "fantasy"],
   },
   {
     id: 2,
     title: "Book-2",
     author: "Author-2",
+    genres: ["science fiction", "humor", "short stories"],
   },
   {
     id: 3,
     title: "Book-3",
     author: "Author-3",
+    genres: ["science fiction", "humor", "fantasy"],
   },
   {
     id: 4,
     title: "Book-4",
     author: "Author-4",
+    genres: ["science fiction", "fantasy"],
   },
   {
     id: 5,
     title: "Book-5",
     author: "Author-5",
+    genres: ["science fiction", "humor"],
   },
 ];
 
@@ -39,4 +44,8 @@ const books = getBooks();
 const book = getBook(2);
 
 // Destructuring object
-const { title, author } = book;
+const { title, author, genres } = book;
+
+// Destructuring array
+
+const [primaryGenre, secondaryGenre] = genres;
