2f882868800187c96731016938100b9ff4238cd5
Ciprian <cozma_ciprian19@yahoo.com>
2024-04-29 17:20:19 +0300

    added template literals

---
+++ javascriptForReact/templateLiterals.js
@@ -0,0 +1,5 @@
+const book = "The Mermaid";
+const author = "Ciprian C";
+const summary = "A nice book";
+
+const about = `${book} is written by ${author} and is seems to be ${summary.toLowerCase()}`;
